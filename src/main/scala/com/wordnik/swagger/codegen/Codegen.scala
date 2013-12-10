7d6883a1913bd98597e65b22d77bdcc596f6b54d
Eric Martineau <ericm@infusionsoft.com>
2013-12-09 23:44:03 -0700

    Adding full return for enyo models

--- src/main/scala/com/wordnik/swagger/codegen/Codegen.scala
+++ src/main/scala/com/wordnik/swagger/codegen/Codegen.scala
@@ -581,7 +581,7 @@ class Codegen(config: CodegenConfig) {
     engine.compiler.shutdown()
   }
 
-  protected def isListType(dt: String) = isCollectionType(dt, "List") || isCollectionType(dt, "Array") || isCollectionType(dt, "Set")
+  protected def isListType(dt: String) = isCollectionType(dt, "Collection") || isCollectionType(dt, "List") || isCollectionType(dt, "Array") || isCollectionType(dt, "Set")
 
   protected def isMapType(dt: String) = isCollectionType(dt, "Map")
 
